b97d8d482f0dad11c551288eb1ee437db91010e5
DaniloZZZ <lkv97dn@gmail.com>
2018-01-21 00:51:54 +0300

    directory cleaned

--- do.sh
+++
@@ -1,8 +0,0 @@
-avr-gcc  -mmcu=atmega328p -c simtest.c -Wall -g -Os 
-
-avr-gcc  -mmcu=atmega328p -o simtest.elf simtest.o
-
-avr-objcopy  -I ihex blink.elf blink.hex   
-
-avrdude -p atmega328p -c Usbasp -U flash:w:blink.hex  -v
-
